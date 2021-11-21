f4310ccfd8078b3e41b1ab54ffc01a680db2011c
suryansh <suryanshbhar@gmail.com>
2021-11-21 22:24:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -102,7 +102,7 @@ def gen_frames():
             if(score<0):
                 score=0   
             cv2.putText(frame,'Score:'+str(score),(100,height-20), font, 1,(255,255,255),1,cv2.LINE_AA)
-            if(score>30):
+            if(score>13):
                 #person is feeling sleepy so we beep the alarm
                 cv2.imwrite(os.path.join(path,'image.jpg'),frame)
                 # try:
